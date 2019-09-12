fbf9e2dd37eeea0310581809a0277b6882d4279e
Drew Dowling <drewdowling37@gmail.com>
2019-09-12 15:55:03 -0700

    ch06 todo server process exercise

--- code_samples/ch06/test/tests.exs
+++ code_samples/ch06/test/tests.exs
@@ -43,6 +43,16 @@ defmodule Test do
              TodoServer.entries(pid, ~D[2018-01-01])
   end
 
+  test_script "my_server_process_todo" do
+    pid = TodoServer.start()
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
+
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] ==
+             TodoServer.entries(pid, ~D[2018-01-01])
+  end
+
   test_script "todo_server" do
     {:ok, pid} = TodoServer.start()
     TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
