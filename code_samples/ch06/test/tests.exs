c857423d3b84915309a6d8e33a3a542927c0f324
Drew Dowling <drewdowling37@gmail.com>
2019-09-12 18:05:46 -0700

    todo server with GenServer

--- code_samples/ch06/test/tests.exs
+++ code_samples/ch06/test/tests.exs
@@ -62,4 +62,14 @@ defmodule Test do
     assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] ==
              TodoServer.entries(pid, ~D[2018-01-01])
   end
+
+  test_script "my_todo_server" do
+    {:ok, pid} = TodoServer.start()
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
+
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] ==
+             TodoServer.entries(pid, ~D[2018-01-01])
+  end
 end
